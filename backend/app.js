26534f318251766d18c3828374d3d46b3420ca71
SarpKantar <fapsara@hotmail.com>
2024-08-19 19:13:18 +0300

    first commit

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const userRoutes = require('./routes/userRoutes');
-
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.use('/api/users', userRoutes);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
+const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app'); // Import the app.js file
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
+
+const app = express();
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -10,6 +15,8 @@ mongoose.connect(process.env.MONGODB_URI, {
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('MongoDB connection error:', err));
+
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
