7441469d02889f8842ae87ba724f978f2d64ddd9
KeremMus <keremcancu42@gmail.com>
2024-08-31 19:15:05 +0300

    logo added to the home screen

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styling/HomePage.css';
+import METU_DTX_Logo from '../DTX-Logo-Sqr.png'; // Ensure this path is correct
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -24,6 +25,9 @@ const HomePage = () => {
 
   return (
     <div className="container">
+      <div className="logo-container">
+        <img src={METU_DTX_Logo} alt="METU DTX Logo" className="logo" />
+      </div>
       <div className="login-box">
         <h1 className="title-home">Digital Innovation Assessment</h1>
         {!showRegisterOptions ? (
