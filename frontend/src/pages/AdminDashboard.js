352d07920b88264a57beac87bfc02247193d2786
Sarp Kantar <fapsara@hotmail.com>
2024-09-15 20:49:06 +0300

    Updated.

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -188,7 +188,7 @@ const AdminDashboard = () => {
         <h2>Pending Requests</h2>
         <h3>Company Requests</h3>
         {pendingCompanyRequests.length === 0 ? (
-          <p className="no-requests-message">No company requests available.</p>
+          <p >No company requests available.</p>
         ) : (
           pendingCompanyRequests.map(request => (
             <div key={request.id} className="request-item" onClick={() => handleUserClick(request)}>
